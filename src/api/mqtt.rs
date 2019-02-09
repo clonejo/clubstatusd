9f06bb5fda4822e63d4370d9ab536228f3577d88
clonejo <clonejo@shakik.de>
2019-02-09 02:08:03 +0100

    update rumqtt to 0.30.1

--- src/api/mqtt.rs
+++ src/api/mqtt.rs
@@ -4,12 +4,12 @@ use std::sync::mpsc::{channel, Sender, TryRecvError};
 use std::thread;
 use std::time::Duration;
 
-use rumqtt::{MqttOptions, MqttClient, QoS};
+use rumqtt::{MqttOptions, MqttClient, QoS, ReconnectOptions};
 
 use model::*;
 use db::{DbCon, status};
 
-fn publish_status(action: &StatusAction, mqtt_request: &mut MqttClient, topic_prefix: &str) {
+fn publish_status(action: &StatusAction, mqtt_client: &mut MqttClient, topic_prefix: &str) {
     use rustc_serialize::json::ToJson;
 
     {
@@ -18,19 +18,20 @@ fn publish_status(action: &StatusAction, mqtt_request: &mut MqttClient, topic_pr
             Status::Private => "private",
             Status::Closed => "closed"
         };
-        mqtt_request.retained_publish(format!("{}status", topic_prefix).as_str(), QoS::Level1,
-            payload.into()).unwrap();
+        mqtt_client.publish(format!("{}status", topic_prefix).as_str(), QoS::AtLeastOnce, true,
+            payload).unwrap();
     }
     {
-        mqtt_request.retained_publish(format!("{}status/last", topic_prefix).as_str(), QoS::Level1,
-            action.to_json().to_string().into()).unwrap();
+        mqtt_client.publish(format!("{}status/last", topic_prefix).as_str(), QoS::AtLeastOnce, true,
+            action.to_json().to_string()).unwrap();
     }
 
 }
 
-fn publish_announcement(_action: &AnnouncementAction, mqtt_request: &mut MqttClient, topic_prefix: &str) {
-        mqtt_request.publish(
-            format!("{}announcement", topic_prefix).as_str(), QoS::Level1, "not_implemented".into()).unwrap();
+fn publish_announcement(_action: &AnnouncementAction, mqtt_client: &mut MqttClient, topic_prefix: &str) {
+        mqtt_client.publish(
+            format!("{}announcement", topic_prefix).as_str(), QoS::AtLeastOnce, false,
+            "not_implemented").unwrap();
 }
 
 fn publish_presence<'a>(action: &'a PresenceAction, client: &mut MqttClient, topic_prefix: &str) {
@@ -43,7 +44,7 @@ fn publish_presence<'a>(action: &'a PresenceAction, client: &mut MqttClient, top
         }).collect();
     users.sort();
     let users_string: String = users.join(",");
-    client.retained_publish(format!("{}presence/list", topic_prefix).as_str(), QoS::Level1, users_string.into()).unwrap();
+    client.publish(format!("{}presence/list", topic_prefix).as_str(), QoS::AtLeastOnce, true, users_string).unwrap();
     for user in action.users.iter() {
         let status_str = match user.status {
             PresentUserStatus::Joined  => "joined",
@@ -51,7 +52,8 @@ fn publish_presence<'a>(action: &'a PresenceAction, client: &mut MqttClient, top
             PresentUserStatus::Left    => "left"
         };
         client.publish(
-            format!("{}presence/{}/{}", topic_prefix, status_str, user.name).as_str(), QoS::Level2, user.since.to_string().into()).unwrap();
+            format!("{}presence/{}/{}", topic_prefix, status_str, user.name).as_str(), QoS::ExactlyOnce,
+            false, user.since.to_string()).unwrap();
     }
 }
 
@@ -62,11 +64,10 @@ pub fn start_handler(server: Option<String>, topic_prefix: String, shared_con: A
     match server {
         Some(server_str) => {
             thread::Builder::new().name(String::from("mqtt_client")).spawn(move || {
-                let opts = MqttOptions::new()
+                let opts = MqttOptions::new("clubstatusd", server_str, 1883)
                     .set_keep_alive(30)
-                    .set_reconnect(30)
-                    .set_broker(server_str.as_str());
-                let mut mqtt_request = MqttClient::start(opts, None)
+                    .set_reconnect_opts(ReconnectOptions::Always(30));
+                let (mut mqtt_client, _mqtt_receiver) = MqttClient::start(opts)
                     .expect("could not connect to mqtt server");
                 println!("connected to mqtt server");
 
@@ -74,7 +75,7 @@ pub fn start_handler(server: Option<String>, topic_prefix: String, shared_con: A
                     let con = shared_con.lock().unwrap();
                     status::get_last(&*con).unwrap()
                 };
-                publish_status(&last_status, &mut mqtt_request, &*topic_prefix);
+                publish_status(&last_status, &mut mqtt_client, &*topic_prefix);
 
                 loop {
                     loop {
@@ -82,13 +83,13 @@ pub fn start_handler(server: Option<String>, topic_prefix: String, shared_con: A
                             Ok(msg) => {
                                 match msg {
                                     TypedAction::Status(action) => {
-                                        publish_status(&action, &mut mqtt_request, &*topic_prefix);
+                                        publish_status(&action, &mut mqtt_client, &*topic_prefix);
                                     },
                                     TypedAction::Announcement(action) => {
-                                        publish_announcement(&action, &mut mqtt_request, &*topic_prefix);
+                                        publish_announcement(&action, &mut mqtt_client, &*topic_prefix);
                                     },
                                     TypedAction::Presence(action) => {
-                                        publish_presence(&action, &mut mqtt_request, &*topic_prefix);
+                                        publish_presence(&action, &mut mqtt_client, &*topic_prefix);
                                     }
                                 }
                             },
